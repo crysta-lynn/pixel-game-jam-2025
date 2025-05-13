e5f05707934b8d1676efaf2bc6f228100571d52d
honey <crystadarling@yahoo.com>
2025-05-13 00:50:35 -0800

    character is now animated while walking

--- Assets/Scripts/character_body_2d.gd
+++ Assets/Scripts/character_body_2d.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var living_land: TileMapLayer = $"../TileMap/living land"
 @onready var ghost_land: TileMapLayer = $"../TileMap/ghost land"
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var speed := 10.0
 
@@ -11,34 +12,49 @@ var living := true
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
+	if Input.is_action_just_pressed("swapWorld"):
+		living = !living
+		swapMap()
 
 func player_movement(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = speed*speedMultiplier
 		velocity.y = 0
+		if isLiving():
+			animated_sprite_2d.play("alive walk right")
+		else:
+			animated_sprite_2d.play("ghost walk right")
 	elif Input.is_action_pressed("left"):
 		velocity.x = -speed*speedMultiplier
 		velocity.y = 0
+		if isLiving():
+			animated_sprite_2d.play("alive walk left")
+		else:
+			animated_sprite_2d.play("ghost walk left")
 	elif Input.is_action_pressed("down"):
 		velocity.x = 0
 		velocity.y = speed*speedMultiplier
+		if isLiving():
+			animated_sprite_2d.play("alive walk down")
+		else:
+			animated_sprite_2d.play("ghost walk down")
 	elif Input.is_action_pressed("up"):
 		velocity.x = 0
 		velocity.y = -speed*speedMultiplier
+		if isLiving():
+			animated_sprite_2d.play("alive walk up")
+		else:
+			animated_sprite_2d.play("ghost walk up")
 	else:
 		velocity.x = 0
 		velocity.y = 0
+		if isLiving():
+			animated_sprite_2d.stop()
 	move_and_slide()
 	
 func isLiving():
-	if Input.is_action_pressed("swapWorld"):
-		living = !living
 	return living
 
 func swapMap():
-	if isLiving():
-		living_land.visible
-		ghost_land.hide
-	if !isLiving():
-		living_land.hide
-		ghost_land.visible
+	living_land.visible = living
+	ghost_land.visible = !living
